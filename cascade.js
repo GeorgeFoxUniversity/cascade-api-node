5a345d87c3d527902ccecd4957597e30db2f5e9b
Peter Crackenberg <pcrackenberg@georgefox.edu>
2020-07-29 10:12:17 -0700

    Reworked how the api uses authentication and identifies assets. Also added a helper class to create blank assets, and added comments.

--- cascade.js
+++ cascade.js
@@ -1,62 +1,135 @@
 const fetch = require('node-fetch');
 
+/**
+ * Class to wrap the Cascade Server API.
+ */
 class Cascade
 {
-    constructor(apiRoot, username, password, site="")
+    /**
+     * Create a new Cascade object.
+     * @param {*} apiRoot Root URL of your Cascade instance
+     * @param {*} authentication Authentication object, should either have username and password properties or apiKey property
+     * @param {*} site Optional. Site to use if using paths to identify assets.
+     */
+    constructor(apiRoot, authentication, site="")
     {
         this.apiRoot = apiRoot;
         this.site = site;
-        this.authentication = {
-            username:username,
-            password:password
-        };
+        if(site != "")
+        {
+            this.identificationMode = "path";
+        }
+        else
+        {
+            this.identificationMode = "id";
+        }
+        this.authentication = authentication;
         this.ajaxOptions = {method:"POST"};
         this.ajaxURL = "";
-        this.blankAsset = {
-            name: "",
-            parentFolderPath: "",
-            siteName: "",
-            xhtml: "",
-            metadata: {
-                title: "",
-                displayName: ""
-            }
-        }
     }
 
+    /**** Mode Functions ****/
+
+    /**
+     * Change the mode of the Cascade object to use paths.
+     * @param {*} siteName Name of the site to add to calls.
+     */
+    usePath(siteName)
+    {
+        this.site = siteName;
+        this.identificationMode = "path";
+    }
+
+    /**
+     * Change the mode of the Casade object to use ids.
+     */
+    useId()
+    {
+        this.identificationMode = "id";
+    }
+
+    /**** Utility Functions ****/
+
+    /**
+     * Return a blank page, this is helpful when creating new assets.
+     */
+    createBlankPage()
+    {   
+        return new CascadeAsset("page");
+    }
+
+    /**
+     * Return a blank file, this is helpful when creating new assets.
+     */
+    createBlankFile()
+    {
+        return new CascadeAsset("file");
+    }
+
+    /**** API Functions ****/
+
+    /**
+     * Make an API call. Using the pre-defined functions is generally preferred, but if not all APIs
+     * have wrapper functions, this can be called directly. While most parameters are optional,
+     * most calls will need some number of them to be included.
+     * 
+     * This function is asynchronous, and will either return an object, or throw an error if the call
+     * did not succeed. The error should be caught on the front end.
+     * 
+     * @param {*} operation Name of the operation as defined in the Cascade API.
+     * @param {*} type Required for calls where an identifier is needed (read, publish, etc)
+     * @param {*} assetIDPath Either the path or id of the asset, needed where an identifier is needed, like type
+     * @param {*} assetObject Asset to be acted upon, needed for some calls (create, edit)
+     */
     async APICall(operation,type=false,assetIDPath=false,assetObject=false)
     {
+        // Build the url to call
         this.ajaxURL = this.apiRoot+"/api/v1/"+operation;
         
+        // Add type if included
         if(type !== false)
         {
             this.ajaxURL += "/"+type;
         }
 
-        if(this.site != "")
+        // If using paths, add site
+        if(this.identificationMode == "path")
         {
             this.ajaxURL += "/"+encodeURIComponent(this.site);
         }
 
+        // If an ID or path was included, add it
         if(assetIDPath !== false)
         {
-            this.ajaxURL += "/"+assetIDPath;
+            if(assetIDPath.startsWith("/"))
+            {
+                this.ajaxURL += assetIDPath;
+            }
+            else
+            {
+                this.ajaxURL += "/"+assetIDPath;
+            }
         }
 
+        // Do a post, incluing the authentication in the body
         this.ajaxOptions = {method:"POST"};
         this.ajaxOptions.body = {};
         this.ajaxOptions.body.authentication = this.authentication;
 
+        // If we're including an asset, add it to the body
         if(assetObject !== false)
         {
             this.ajaxOptions.body.asset = assetObject;
         }
 
+        // Turn the body message into a JSON string to send it along
         this.ajaxOptions.body = JSON.stringify(this.ajaxOptions.body);
 
+        // Make the API call, waiting for the result
         const readResult = await fetch(this.ajaxURL, this.ajaxOptions);
         const readData = await readResult.json();
         
+        // Return the value on a success, or throw an error if not
         if(readData.success === true)
         {
             return readData;
@@ -67,6 +140,10 @@ class Cascade
         }
     }
 
+    /**** Wrapper functions ****/
+    /**
+     * These functions all use the APICall function with various options, to make it easier to use.
+     */
     async readPage(path)
     {
         return await this.APICall("read","page",path);
@@ -117,18 +194,39 @@ class Cascade
         return await this.APICall("create",false,false,{file: assetObject});
     }
 
-    createBlankPage()
-    {
-        const page = this.blankAsset;
-        page.contentTypeId = "";
-        return page;
-    }
+}
 
-    createBlankFile()
+/**
+ * Class to represent assets to submit into Cascade Server. These are generally used
+ * when creating a new asset, as it makes it easier than creating the required 
+ * properties from scratch.
+ */
+class CascadeAsset
+{
+    /**
+     * Create a new CascadeAsset object.
+     * @param {*} type Either page or file, defaults to page.
+     */
+    constructor(type="page")
     {
-        const file = this.blankAsset;
-        file.data = [];
-        return file;
+        this.name = "";
+        this.parentFolderPath = "";
+        this.siteName = "";
+
+        if(type == "page")
+        {
+            this.xhtml = "";
+            this.contentTypeId = "";
+            this.metadata = {
+                        title: "",
+                        displayName: ""
+                    };
+        }
+        else if(type == "file")
+        {
+            this.data = [];
+            this.text = "";
+        }
     }
 }
 
